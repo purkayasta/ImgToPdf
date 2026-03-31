29c2c366ebd9c67291dbe187e35373fa657bd3ea
purkayasta <pritompurkayasta@gmail.com>
2026-04-01 02:52:59 +0600

    fixed gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import tailwindcss from '@tailwindcss/vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+  base: '/',
   plugins: [solid(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
